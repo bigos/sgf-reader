6205529a3f49751cb723e5d762b8bed3b661bb1b
Jacek Podkanski <ruby.object@googlemail.com>
2017-02-06 13:18:50 +0000

    generated new project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sgf-reader
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/sgf_reader/core.clj
@@ -0,0 +1,7 @@
+(ns sgf-reader.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
