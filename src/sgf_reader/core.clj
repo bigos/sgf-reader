c05646847666a6cc726497caaea08059de856a54
Jacek Podkanski <ruby.object@googlemail.com>
2017-02-06 15:56:05 +0000

    simple seesaw example

--- src/sgf_reader/core.clj
+++ src/sgf_reader/core.clj
@@ -1,7 +1,17 @@
 (ns sgf-reader.core
-  (:gen-class))
+  (:use seesaw.core)
+  (:gen-class)
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Good bye javaFX!")
+  (native!)
+  (invoke-later
+   (-> (frame :title "Hello",
+              :content "Hello, hello hello my dear Seesaw",
+              :on-close :exit)
+       pack!
+       show!))
+  )
