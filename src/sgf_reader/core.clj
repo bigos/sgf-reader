3e6c0c2bf61c80e8f4f235773eb5582546b83f66
Jacek Podkanski <ruby.object@googlemail.com>
2017-02-22 00:03:44 +0000

    stuck at defining grammar for new lines

--- src/sgf_reader/core.clj
+++ src/sgf_reader/core.clj
@@ -1,4 +1,5 @@
 (ns sgf-reader.core
+  ;; http://stackoverflow.com/questions/17432282/clojures-require-and-instaparse
   (:require [instaparse.core :as insta])
   (:use seesaw.core
         seesaw.chooser
@@ -24,10 +25,13 @@
                           ["Folders" #(.isDirectory %)]
                           (file-filter "All files" (constantly true))]]))
 
+;; (defn file-content
+;;   [file-name]
+;;   (let [in (input-stream file-name)]
+;;       (cl-format true "=== ~A ~A~%" in *in*)))
 (defn file-content
   [file-name]
-  (let [in (input-stream file-name)]
-      (cl-format true "=== ~A ~A~%" in *in*)))
+  (slurp file-name))
 
 ;; http://stackoverflow.com/questions/17432282/clojures-require-and-instaparse
 (def as-and-bs
@@ -36,3 +40,29 @@
      AB = A B
      A = 'a'+
      B = 'b'+"))
+
+(def sgf-data
+  (insta/parser
+   "S = GameTree+
+  GameTree   = '(' Sequence GameTree* ')'
+  Sequence   = Node+
+  Node       = Newline? ';' Property* Newline?
+  Property   = ( (Newline)? PropIdent PropValue+)+ Newline?
+  PropIdent  = UcLetter+
+  PropValue  = '[' CValueType ']'
+  CValueType = (ValueType | Compose)
+  Newline = #\"$\"
+  Digit = ('0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9')
+  Digits = Digit+
+  Number = ('+' '-')? Digits
+  Real = Number '.' Digits
+  Double = Real 'e' Real
+  Color = 'white' 'black'
+  SimpleText = (#\".\")+
+  Text = SimpleText
+  Point = (LcLetter LcLetter)
+  ValueType  = ( Number | Real | Double | Color | SimpleText | Text )?
+  LcLetter = #\"[a-z]\"
+  UcLetter   = ('A'|'B'|'C'|'D'|'E'|'F'|'G'|'H'|'I'|'J'|'K'|'L'|'M'|'N'|'O'|'P'|'Q'|'R'|'S'|'T'|'U'|'V'|'W'|'X'|'Y'|'Z')
+  Compose    = ValueType ':' ValueType
+   "))
